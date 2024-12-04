eed6a2e1284187b6e8c98f6a7c41b10c0b5e2ffa
ShahadAlkazzaz <shahad.alqazzaz@yahoo.com>
2024-12-04 23:03:49 +0100

    Installing och rensning av React-projekt

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      {/* Här kommer dina komponenter att läggas */}
+      <h1>Welcome to My Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
